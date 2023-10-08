e1d6501c2f030f82c933bc1d8bbb09b458a9d039
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-10-08 20:09:48 +0300

    Update tests_controller.rb

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,13 +1,17 @@
 class TestsController < Simpler::Controller
 
   def index
-   render plain: "Plain text response"
+  render plain: "text plain"
    status 400
    headers['Content-Type'] = 'text/'
   end
 
   def create
 
+  end
+
+  def show
+   @test = Test.find(params)
   end
 
 end
